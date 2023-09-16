a7303052539de0bfeb8555d11d76ca6e87fa7d28
Anton Yakutovich <drakulavich@gmail.com>
2023-09-16 19:15:17 +0400

    Happy linters

--- tests/plugins/identity/user.py
+++ tests/plugins/identity/user.py
@@ -5,9 +5,11 @@ from typing import Callable, Protocol, TypeAlias, TypedDict, Unpack, final
 class UserData(TypedDict, total=False):
     """
     Represent the simplified user data that is required to create a new user.
+
     It does not include ``password``, because it is very special in django.
     Importing this type is only allowed under ``if TYPE_CHECKING`` in tests.
     """
+
     email: str
     first_name: str
     last_name: str
@@ -22,19 +24,21 @@ class UserData(TypedDict, total=False):
 class RegistrationData(UserData, total=False):
     """
     Represent the registration data that is required to create a new user.
+
     Importing this type is only allowed under ``if TYPE_CHECKING`` in tests.
     """
+
     password1: str
     password2: str
 
 
 @final
 class RegistrationDataFactory(Protocol):
-	def __call__(
-		self,
-		**fields: Unpack[RegistrationData],
-	) -> RegistrationData:
-		"""User data factory protocol."""
+    def __call__(
+        self,
+        **fields: Unpack[RegistrationData],
+    ) -> RegistrationData:
+        """User data factory protocol."""
 
 
 UserAssertion: TypeAlias = Callable[[str, UserData], None]
--- tests/test_apps/test_identity/test_registration.py
+++ tests/test_apps/test_identity/test_registration.py
@@ -1,5 +1,5 @@
-from http import HTTPStatus
 import random
+from http import HTTPStatus
 from typing import Unpack
 
 import pytest
@@ -7,7 +7,6 @@ from django.test import Client
 from django.urls import reverse
 from mimesis.locales import Locale
 from mimesis.schema import Field, Schema
-
 from plugins.identity.user import (
     RegistrationData,
     RegistrationDataFactory,
@@ -20,80 +19,94 @@ from server.apps.identity.models import User
 
 @pytest.fixture(scope='session')
 def assert_correct_user() -> UserAssertion:
-	def factory(email: str, expected: UserData) -> None:
-		user = User.objects.get(email=email)
-		# Special fields:
-		assert user.id
-		assert user.is_active
-		assert not user.is_superuser
-		assert not user.is_staff
-		# All other fields:
-		for field_name, data_value in expected.items():
-			assert getattr(user, field_name) == data_value
-	return factory
+    def factory(email: str, expected: UserData) -> None:
+        user = User.objects.get(email=email)
+        # Special fields:
+        assert user.id
+        assert user.is_active
+        assert not user.is_superuser
+        assert not user.is_staff
+        # All other fields:
+        for field_name, data_value in expected.items():
+            assert getattr(user, field_name) == data_value
+
+    return factory
+
 
 @pytest.fixture()
 def registration_data_factory(faker_seed: int) -> RegistrationDataFactory:
-	"""Returns factory for fake random data for regitration."""
-	def factory(**fields: Unpack[RegistrationData]) -> RegistrationData:
-		mf = Field(locale=Locale.RU, seed=faker_seed)
-		password = mf('password') # by default passwords are equal
-		schema = Schema(schema=lambda: {
-            'email': mf('person.email'),
-            'first_name': mf('person.first_name'),
-            'last_name': mf('person.last_name'),
-            'date_of_birth': mf('datetime.date'),
-            'address': mf('address.city'),
-            'job_title': mf('person.occupation'),
-            'phone': mf('person.telephone'),
-        }, iterations=1)
-		return {
-			**schema.create()[0], # type: ignore[misc]
-			**{'password1': password, 'password2': password},
-			**fields,
-		}
-	return factory
+    """Returns factory for fake random data for regitration."""
 
-@pytest.fixture(scope="session")
+    def factory(**fields: Unpack[RegistrationData]) -> RegistrationData:
+        mf = Field(locale=Locale.RU, seed=faker_seed)
+        password = mf('password')  # by default passwords are equal
+        schema = Schema(
+            schema=lambda: {
+                'email': mf('person.email'),
+                'first_name': mf('person.first_name'),
+                'last_name': mf('person.last_name'),
+                'date_of_birth': mf('datetime.date'),
+                'address': mf('address.city'),
+                'job_title': mf('person.occupation'),
+                'phone': mf('person.telephone'),
+            },
+            iterations=1,
+        )
+        return {
+            **schema.create()[0],  # type: ignore[misc]
+            **{'password1': password, 'password2': password},
+            **fields,
+        }
+
+    return factory
+
+
+@pytest.fixture(scope='session')
 def faker_seed():
-    return random.Random().getrandbits(32)
+    bits_count = 32
+    return random.Random().getrandbits(bits_count)
+
 
 @pytest.fixture()
 def registration_data(
     registration_data_factory: RegistrationDataFactory,
 ) -> RegistrationData:
-	"""Returns fake random data for regitration."""
-	return registration_data_factory()
+    """Returns fake random data for regitration."""
+    return registration_data_factory()
+
 
 @pytest.fixture()
 def user_data(registration_data: 'RegistrationData') -> 'UserData':
-	"""
-	We need to simplify registration data to drop passwords.
-	Basically, it is the same as ``registration_data``, but without passwords.
-	"""
-	return { # type: ignore[return-value]
-		key_name: value_part
-		for key_name, value_part in registration_data.items()
-		if not key_name.startswith('password')
-	}
+    """
+    We need to simplify registration data to drop passwords.
+
+    Basically, it is the same as ``registration_data``, but without passwords.
+    """
+    return {  # type: ignore[return-value]
+        key_name: value_part
+        for key_name, value_part in registration_data.items()
+        if not key_name.startswith('password')
+    }
+
 
 @pytest.fixture()
 def expected_user_data(user_data: RegistrationData):
     return user_data
 
+
 @pytest.mark.django_db()
 def test_valid_registration(
-	client: Client,
-	registration_data: 'RegistrationData',
-	expected_user_data: 'UserData',
-	assert_correct_user: 'UserAssertion',
+    client: Client,
+    registration_data: 'RegistrationData',
+    expected_user_data: 'UserData',
+    assert_correct_user: 'UserAssertion',
 ) -> None:
-	"""Test that registration works with correct user data."""
-	response = client.post(
-		reverse('identity:registration'),
-		data=registration_data,
-	)
+    """Test that registration works with correct user data."""
+    response = client.post(
+        reverse('identity:registration'),
+        data=registration_data,
+    )
 
-	assert response.status_code == HTTPStatus.FOUND
-	assert response.get('Location') == reverse('identity:login')
-	assert_correct_user(registration_data['email'], expected_user_data)
+    assert response.status_code == HTTPStatus.FOUND
+    assert response.get('Location') == reverse('identity:login')
+    assert_correct_user(registration_data['email'], expected_user_data)
